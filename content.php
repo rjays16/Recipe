8dfaf2b90f1c24e298db14fce449cc54a73859d1
allan81 <rjaycondiman81@gmail.com>
2024-04-27 05:06:26 +0800

    Initial push for container or main content

---
+++ content.php
@@ -0,0 +1,33 @@
+<style>
+.left-side {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-right: 100px;
+}
+
+.left-side img {
+    margin-top: 5px; /* Adjust as needed */
+}
+
+</style>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="left-side d-flex flex-column align-items-center">
+                <p class="mb-0">SHARE:</p>
+                <img src="Images/social_media/facebook.png" alt="Facebook"/>
+                <img src="Images/social_media/twitter.png" alt="Twitter"/>
+                <img src="Images/social_media/whatsapp.png" alt="WhatsApp"/>
+                <img src="Images/social_media/pinterest.png" alt="Pinterest"/>
+                <img src="Images/social_media/link.png" alt="Link"/>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="center"></div>
+        </div>
+        <div class="col-md-3">
+            <div class="right-side"></div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <?php include('navbar.php'); ?>
   <hr>
   <?php include('breadcrum.php'); ?>
-
+  <?php include('content.php'); ?>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>

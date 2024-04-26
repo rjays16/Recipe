e995d6530fb6650c82f509d99c8bc26ca04206d9
allan81 <rjaycondiman81@gmail.com>
2024-04-27 04:49:38 +0800

    Add breadcrum

---
+++ breadcrum.php
@@ -0,0 +1,13 @@
+<style>
+    .path u{
+        font-size: 16px;
+        text-decoration: underline;
+        text-decoration-color: #E5B64C;
+        line-height: 3px;
+    }
+</style>
+
+<div class="container path">
+    <u>Recipes</u> > <u>Desert</u> > <u>Berry Tiramisu Cake Recipe</u>
+
+</div>
--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 <body>
   <?php include('header.php'); ?>
   <?php include('navbar.php'); ?>
+  <hr>
+  <?php include('breadcrum.php'); ?>
 
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
